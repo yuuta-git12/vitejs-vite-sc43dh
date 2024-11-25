e54058367d38c9881e7cab0695ae57dd9b38ca03
yuuta-git12 <tayu2201@icloud.com>
2024-11-25 11:18:33 +0900

    map、filterは使った配列の処理追加

--- src/index.js
+++ src/index.js
@@ -169,32 +169,73 @@
  */
 
 // 配列の展開
-const arr1 = [1,2];
-console.log(arr1);
-console.log(...arr1); // スプレッド構文で配列が展開される
+// const arr1 = [1,2];
+// console.log(arr1);
+// console.log(...arr1); // スプレッド構文で配列が展開される
 
-// 引数の全ての要素の和を返す関数
-const sumFunc = (...nums) => console.log(nums.reduce((num1,num2) => num1 + num2));
-sumFunc(10,20,...arr1);
+// // 引数の全ての要素の和を返す関数
+// const sumFunc = (...nums) => console.log(nums.reduce((num1,num2) => num1 + num2));
+// sumFunc(10,20,...arr1);
 
-// まとめる
-const arr2 = [1,2,3,4,5];
-// 1,2はnum1,num2,　残りはnumsに代入
-const [num1,num2, ...nums] = arr2;
-console.log(num1);
-console.log(num2);
-console.log(nums);
+// // まとめる
+// const arr2 = [1,2,3,4,5];
+// // 1,2はnum1,num2,　残りはnumsに代入
+// const [num1,num2, ...nums] = arr2;
+// console.log(num1);
+// console.log(num2);
+// console.log(nums);
 
-// 配列のコピー、結合
-const arr4 = [10,20];
-const arr5 = [30,40];
+// // 配列のコピー、結合
+// const arr4 = [10,20];
+// const arr5 = [30,40];
 
-// arr4のコピー(値渡し コピー元の値は変わらない)
-const arr6 = [...arr4];
-arr6[0] = 100;
-console.log(arr6);
-console.log(arr4);
+// // arr4のコピー(値渡し コピー元の値は変わらない)
+// const arr6 = [...arr4];
+// arr6[0] = 100;
+// console.log(arr6);
+// console.log(arr4);
 
-// arr4とarr5を結合したarr7(値渡し)
-const arr7 = [...arr4,...arr5];
-console.log(arr7);
+// // arr4とarr5を結合したarr7(値渡し)
+// const arr7 = [...arr4,...arr5];
+// console.log(arr7);
+
+/**
+ * mapやfilterを使った配列の処理
+ */
+const nameArr = ["田中","山田","じゃけぇ"];
+
+// 従来の配列要素の表示方法
+// for (let index = 0; index < nameArr.length; index++) {
+//     console.log(`${index + 1}番目は${nameArr[index]}です`);
+// }
+
+// mapを使用した方法 第二引数にインデックス(要素番号)を入れることができる
+// nameArr.map((name) => {
+//     console.log(name);
+// });
+// nameArr.map((name, index) => console.log(`${index + 1}番目は${name}です`));
+
+// const nameArr2 = nameArr.map((name) => {
+//     return `${name}さん`;
+// })
+// console.log(nameArr2);
+
+const newNameArr = nameArr.map((name) => {
+    if(name !== "じゃけぇ"){
+        return `${name}さん`
+    }else{
+        return name;
+    }
+})
+console.log(newNameArr);
+
+
+
+// filter
+// const numArr = [1,2,3,4,5];
+// // 奇数の値を抜き出すfilterの使い方
+// const newNumArr = numArr.filter((num) => {
+//     return num % 2 === 1;
+// });
+// console.log(newNumArr);
+
